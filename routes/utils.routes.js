18f31c50d91c9af1e0f0c28b407c10d68c94a505
Amogh <87270666+amogs4165@users.noreply.github.com>
2023-03-26 14:02:03 +0530

    minor change

--- routes/utils.routes.js
+++ routes/utils.routes.js
@@ -3,6 +3,6 @@ import { validateAccessToken } from "../middlewares/auth.js"
 import trim from "../middlewares/trim.js"
 import { getSignedUrl } from "../controllers/utils.controller.js"
 const router=express.Router()
-router.route("/pre-signed-url/:key")
+router.route("/pre-signed-url")
     .get(validateAccessToken,trim,getSignedUrl)
 export default router
